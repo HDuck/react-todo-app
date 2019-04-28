b64fa02fa84c6399f2df28b9de64d521f2f6c0b7
HDuck <1ntranc3d@gmail.com>
2019-04-28 20:16:18 +0300

    preeeetieeer

--- src/index.js
+++ src/index.js
@@ -2,9 +2,9 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 
 class Welcome extends React.Component {
-    render() {
-        return <h1>Welcome to my TODO list app</h1>;
-    }
+	render() {
+		return <h1>Welcome to my TODO list app</h1>;
+	}
 }
 
 ReactDOM.render(<Welcome />, document.getElementById('app'));
