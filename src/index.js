5ca0858d5edf24b44edf560626f435c075342afe
HDuck <1ntranc3d@gmail.com>
2019-05-02 13:54:39 +0300

    editing

--- src/index.js
+++ src/index.js
@@ -1,10 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-class Welcome extends React.Component {
-	render() {
-		return <h1>Welcome to my TODO list app</h1>;
-	}
-}
+const Welcome = () => <h1>Welcome to my TODO list app</h1>;
 
 ReactDOM.render(<Welcome />, document.getElementById('app'));
+
+export default Welcome;
--- webpack.config.js
+++ webpack.config.js
@@ -3,28 +3,28 @@ const HtmlWebpackPlugin = require('html-webpack-plugin');
 const CleanWebpackPlugin = require('clean-webpack-plugin');
 
 module.exports = {
-	context: path.resolve(__dirname, 'src'),
-	entry: './index.js',
-	output: {
-		path: path.resolve(__dirname, 'dist'),
-		filename: 'bundle.js',
-	},
-	devServer: {
-		contentBase: './dist',
-	},
-	plugins: [
-		new HtmlWebpackPlugin({
-			template: './index.html',
-		}),
-		new CleanWebpackPlugin(),
-	],
-	module: {
-		rules: [
-			{
-				test: /\.(js|jsx)$/,
-				exclude: /node_modules/,
-				use: ['babel-loader', 'eslint-loader'],
-			},
-		],
-	},
+  context: path.resolve(__dirname, 'src'),
+  entry: './index.js',
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'bundle.js',
+  },
+  devServer: {
+    contentBase: './dist',
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './index.html',
+    }),
+    new CleanWebpackPlugin(),
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: ['babel-loader', 'eslint-loader'],
+      },
+    ],
+  },
 };
